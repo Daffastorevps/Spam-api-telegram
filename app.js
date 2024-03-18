fe79d35a6e38e8a388255cf8e30397fcfaf0ce97
Daffastorevps <pratamadaffa63136@gmail.com>
2024-03-18 23:12:02 +0700

    hacker

---
+++ app.js
@@ -0,0 +1,51 @@
+const {default: axios} = require("axios")
+
+const url = "https://api.telegram.org/bot6861974788:AAH_AbVplVpEG3XTRhVIgbMVqIeT-_y7vPQ/sendMessage?parse_mode=markdown&chat_id=6951548817&text=taruh teks yg mau di spam di sini"
+
+async function sendMessage() {
+    try {
+        while (true) {
+            await axios.get(url)
+                .then((response) => {
+                    if (response.status == 200) {
+                        console.log(response)
+                    }
+                })
+        }
+    } catch (error) {
+        console.log("API down!")
+        sendMessage();
+    }
+}
+
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
+sendMessage();
